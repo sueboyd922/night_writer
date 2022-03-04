59a62b61d9a409de3ef6e69b0aff4271daba316a
Susan Boyd <sueboyd922@gmail.com>
2022-03-04 16:20:27 -0500

    Test: write test for switching dictionary language

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,7 +1,10 @@
 
 class Dictionary
+  attr_reader :language
 
-
+  def initialize(language = :english)
+    @language = language
+  end
 
 
 end
--- spec/dictionary_spec.rb
+++ spec/dictionary_spec.rb
@@ -10,4 +10,12 @@ describe Dictionary do
     expect(@dictionary).to be_an_instance_of(Dictionary)
   end
 
+  it 'defaults to English language' do
+    expect(@dictionary.language).to eq(:english)
+  end
+
+  it 'can switch to Braille language' do
+    @dictionary.switch
+    expect(@dictionary.language).to eq(:braille)
+  end
 end
