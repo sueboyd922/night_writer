9f41015f145461071a6b916e0d9ee8a0af04318f
Susan Boyd <sueboyd922@gmail.com>
2022-03-04 11:34:40 -0500

    Test: set up test files for spec

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,22 +1,49 @@
 require 'pry'
 
 class NightWriter
-  attr_reader :message, :read_message
+  attr_reader :message, :read_message, :braille
 
   def initialize(file)
     @file = file
-    @message = nil
-    @read_message
+    @message = open_file
+    @read_message = read_file
+    @braille = create_new_file
     # binding.pry
+    run
+  end
+
+  def run
+    read_file
+    create_new_file
+    write_to_new_file
+    print_update
   end
 
   def open_file
-    @message = File.open(@file, 'r')
+     File.open(@file, 'r')
   end
 
   def read_file
     open_file
-    @read_message = @message.read
+     @message.read
+  end
+
+  def create_new_file
+    File.open(ARGV[1], 'w')
+  end
+
+  def write_to_new_file
+    @braille.write(new_message)
+  end
+
+  def new_message
+    @read_message.reverse
+  end
+
+  def print_update
+    puts "Created '#{ARGV[1]}' containing #{@read_message.length} characters"
   end
 
 end
+
+# NightWriter.new(ARGV[0])
--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -3,11 +3,13 @@ require './lib/night_writer'
 
 describe NightWriter do
   before (:each) do
-    @night_writer = NightWriter.new('message.txt')
+    ARGV[1] = './spec/test_files/new_test_message.txt'
+    ARGV[0] = './spec/test_files/test_message.txt'
+    # require "pry"; binding.pry
+    @night_writer = NightWriter.new(ARGV[0])
   end
 
   it 'exists' do
-
     expect(@night_writer).to be_an_instance_of(NightWriter)
   end
 
@@ -20,4 +22,11 @@ describe NightWriter do
     @night_writer.read_file
     expect(@night_writer.read_message.class).to eq(String)
   end
+
+  it 'creates a new file' do
+    @night_writer.create_new_file
+    expect(@night_writer.braille.class).to eq(File)
+  end
+  #
+  # it 'transforms the message '
 end
