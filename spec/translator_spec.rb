13b41ef415aa57015c8d14b6e329c7a5092c4a96
Susan Boyd <sueboyd922@gmail.com>
2022-03-05 11:34:01 -0500

    Test: write test for separating braille letters into lines

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -27,4 +27,14 @@ describe Translator do
     expect(@translator.translate.count).to eq(21)
   end
 
+  it 'separates the braille letters into 3 rows for printing' do
+    @translator.split_braille
+    expect(@translator.top.class).to eq(Array)
+    expect(@translator.top.count).to eq(42)
+    expect(@translator.middle.class).to eq(Array)
+    expect(@translator.middle.count).to eq(42)
+    expect(@translator.bottom.class).to eq(Array)
+    expect(@translator.bottom.count).to eq(42)
+  end
+
 end
