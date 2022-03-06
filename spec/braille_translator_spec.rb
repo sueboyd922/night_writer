38e7a94a2f51905d7e7e7c119fa68014305f5890
Susan Boyd <sueboyd922@gmail.com>
2022-03-06 12:42:37 -0500

    Test: write test for message and breakdown methods for BrailleTranslator

--- spec/braille_translator_spec.rb
+++ spec/braille_translator_spec.rb
@@ -9,4 +9,13 @@ describe BrailleTranslator do
   it 'exists' do
     expect(@translator).to be_an_instance_of(BrailleTranslator)
   end
+
+  it 'takes in a single message to translate' do
+    expect(@translator.message).to eq("OOO..OO..OO.O..O\nO..OOO..OO.O.OO.\nO.O.O...O.O...O.")
+  end
+
+  it 'can break down the message for translation' do
+    expect(@translator.breakdown.class).to eq(Array)
+    expect(@translator.breakdown.count).to eq(8)
+  end
 end
